b779135a4c1f2d38bf76c01ffd66f414d9f2b492
ThomasCode92 <thomascode92@hotmail.com>
2023-11-29 20:42:02 +0100

    CORS Middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const planetsRouter = require('./routes/planets.router');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/planets', planetsRouter);
 
 module.exports = app;
