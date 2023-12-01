ff28dd7197e95521e11161e50d54b54742e02291
ThomasCode92 <thomascode92@hotmail.com>
2023-12-01 22:22:16 +0100

    Logging Requests with Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets.router');
 
@@ -9,9 +10,13 @@ const publicFolder = path.join(__dirname, '..', 'public');
 
 const app = express();
 
+// General middleware
 app.use(express.json());
 app.use(express.static(publicFolder));
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(morgan('combined')); // Request logger
+
+// Routes
 app.use('/planets', planetsRouter);
 
 module.exports = app;
