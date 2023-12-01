2675384f6aaa09c3180eda667ddb83ba626ae153
ThomasCode92 <thomascode92@hotmail.com>
2023-12-01 22:21:55 +0100

    Serving React.js Frontend in Production

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
 
 const planetsRouter = require('./routes/planets.router');
 
+const publicFolder = path.join(__dirname, '..', 'public');
+
 const app = express();
 
 app.use(express.json());
+app.use(express.static(publicFolder));
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/planets', planetsRouter);
 
