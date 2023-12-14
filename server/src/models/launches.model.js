cf815f1bb730a8de5067a2246227e89a8716e0b3
ThomasCode92 <thomascode92@hotmail.com>
2023-12-14 21:56:12 +0100

    POST /launches - Creating Launches (Part 1)

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'kepler exploration x',
@@ -17,4 +19,17 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+
+  const newLaunch = Object.assign(launch, {
+    flightNumber: latestFlightNumber,
+    customer: ['ZTM', 'NASA'],
+    upcoming: true,
+    success: true,
+  });
+
+  launches.set(latestFlightNumber, newLaunch);
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
