fafc2a3d2e82437fb41e46b85215c5c613b11595
ThomasCode92 <thomascode92@hotmail.com>
2023-12-22 07:41:48 +0100

    Testing API Endpoints with 'supertest' - POST Requests

--- server/tests/launches.test.js
+++ server/tests/launches.test.js
@@ -11,9 +11,31 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launch', () => {
-  test('should respond with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-62 f',
+    launchDate: 'January 4, 2028',
+  };
+
+  const launchDataWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-62 f',
+  };
+
+  test('should respond with correct content type and 201 success', async () => {
+    const response = await requests(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+    expect(responseDate).toBe(requestDate);
   });
 
   test('should catch missing required properties', () => {
