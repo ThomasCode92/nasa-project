f26c2bbd8cb77fa183f82c32a1d37b872759f98f
ThomasCode92 <thomascode92@hotmail.com>
2023-12-22 07:50:19 +0100

    Testing API Endpoints with 'supertest' - Error Cases

--- server/tests/launches.test.js
+++ server/tests/launches.test.js
@@ -38,13 +38,27 @@ describe('Test POST /launch', () => {
     expect(responseDate).toBe(requestDate);
   });
 
-  test('should catch missing required properties', () => {
-    const response = 400;
-    expect(response).toBe(400);
+  test('should catch missing required properties', async () => {
+    const response = await requests(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'Missing required launch property',
+    });
   });
 
-  test('should catch invalid dates', () => {
-    const response = 400;
-    expect(response).toBe(400);
+  test('should catch invalid dates', async () => {
+    const response = await requests(app)
+      .post('/launches')
+      .send({ ...completeLaunchData, launchDate: 'hello' })
+      .expect('Content-Type', /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'Invalid launch date',
+    });
   });
 });
