593eb5356805d6c51cfc44c1c707f40257db9b2c
ThomasCode92 <thomascode92@hotmail.com>
2024-02-04 15:35:48 +0100

    Creating Mongoose Schema for Launches

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: { type: Number, required: true, default: 100 },
+  launchDate: { type: Date, required: true },
+  mission: { type: String, required: true },
+  rocket: { type: String, required: true },
+  target: { type: String, required: true },
+  customers: [String],
+  upcoming: { type: Boolean, required: true },
+  success: { type: Boolean, required: true, default: true },
+});
