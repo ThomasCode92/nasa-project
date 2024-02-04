96cadb1bec1aada913e4c5166e09f408776e971d
ThomasCode92 <thomascode92@hotmail.com>
2024-02-04 17:35:43 +0100

    Excluding Fields from the Response

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -47,7 +47,7 @@ function loadPlanetsData() {
 }
 
 async function getAllPlanets() {
-  return await planets.find({});
+  return await planets.find({}, { _id: 0, __v: 0 });
 }
 
 module.exports = { loadPlanetsData, getAllPlanets };
